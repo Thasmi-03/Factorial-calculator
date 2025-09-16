78ddda49e8c3123760121b2e6cde97a540c9166e
Thasmiga <vimalthasmiga1112@gmail.com>
2025-09-16 10:11:40 +0530

    condition  work

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-import {factorial} from './math.js'
+import { factorial } from "./math.js";
 
 function calcu(event) {
   event.preventDefault();
 
   const number1 = document.getElementById("num1").value;
   const result = factorial(number1);
-  console.log(result);
+
+  document.getElementById("resultNumber").innerText = `${number1}!= ${result}`;
 }
 
 const calForm = document.getElementById("calForm");
 calForm.addEventListener("submit", calcu);
+
+
+console.log(factorial());  
+
--- math.js
+++ math.js
@@ -1,4 +1,15 @@
-export function factorial(num1) {
-  let result = parseInt(num1) ;
-  alert(result);
+export function factorial(num) {
+  let n = parseInt(num);
+  if (isNaN(n) || n < 0) {
+    result.value = 'Please enter a valid number!';
+    // result.style.color ="red"
+    return;
+
+  }
+
+  let fact = 1;
+  for (let i = 1; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
 }
